ddbdabd654dd59b94e7662db8b84d894c2e1e774
Nicolas Zarate <horacio.zarate@despegar.com>
2018-06-08 13:06:24 -0300

    creacion de la class public String getDescripcionProveedor() {

---
+++ EjercicioObjetos/Proveedor.java
@@ -0,0 +1,25 @@
+package EjercicioObjetos;
+
+public class Proveedor {
+
+     private long codigoProveedor;
+     private String descripcionProveedor;
+
+     public long getCodigoProveedor() {
+          return codigoProveedor;
+     }
+
+     public void setCodigoProveedor(long codigoProveedor) {
+          this.codigoProveedor = codigoProveedor;
+     }
+
+     public String getDescripcionProveedor() {
+          return descripcionProveedor;
+     }
+
+     public void setDescripcionProveedor(String descripcionProveedor) {
+          this.descripcionProveedor = descripcionProveedor;
+     }
+
+
+}
